93c818f2c98ead0d5dbba3ea1065978d54c8e3ae
Dat <tandat41202gmail.com>
2024-12-29 08:44:38 +0700

    Add project files.

---
+++ Playfair_cipher/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+using System.Drawing.Printing;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace Playfair_cipher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Biến cần thiết cho Designer.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Dọn dẹp các tài nguyên đang được sử dụng.
+        /// </summary>
+        /// <param name="disposing">true nếu các tài nguyên quản lý cần được giải phóng; ngược lại là false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Mã được tạo ra bởi Designer Form
+
+        /// <summary>
+        /// Phương thức yêu cầu hỗ trợ Designer - không sửa đổi nội dung của phương thức này trong trình soạn thảo mã.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(39, 39);
+            textBox1.Margin = new Padding(3, 4, 3, 4);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(413, 49);
+            textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            textBox2.BackColor = SystemColors.Info;
+            textBox2.Font = new System.Drawing.Font("Segoe UI", 25.8000011F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox2.Location = new Point(39, 129);
+            textBox2.Margin = new Padding(3, 4, 3, 4);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(240, 288);
+            textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(490, 39);
+            textBox3.Margin = new Padding(3, 4, 3, 4);
+            textBox3.Multiline = true;
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(497, 154);
+            textBox3.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            textBox4.BackColor = Color.LightYellow;
+            textBox4.Location = new Point(490, 249);
+            textBox4.Margin = new Padding(3, 4, 3, 4);
+            textBox4.Multiline = true;
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(497, 163);
+            textBox4.TabIndex = 3;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.DimGray;
+            button1.Location = new Point(331, 135);
+            button1.Margin = new Padding(3, 4, 3, 4);
+            button1.Name = "button1";
+            button1.Size = new Size(121, 59);
+            button1.TabIndex = 4;
+            button1.Text = "Encrypt";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            button2.BackColor = SystemColors.ControlDarkDark;
+            button2.Location = new Point(331, 238);
+            button2.Margin = new Padding(3, 4, 3, 4);
+            button2.Name = "button2";
+            button2.Size = new Size(121, 59);
+            button2.TabIndex = 5;
+            button2.Text = "Decrypt";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            button3.BackColor = SystemColors.ControlDarkDark;
+            button3.Location = new Point(331, 343);
+            button3.Margin = new Padding(3, 4, 3, 4);
+            button3.Name = "button3";
+            button3.Size = new Size(121, 59);
+            button3.TabIndex = 6;
+            button3.Text = "Clear";
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(36, 11);
+            label1.Name = "label1";
+            label1.Size = new Size(34, 20);
+            label1.TabIndex = 7;
+            label1.Text = "KEY";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(487, 15);
+            label2.Name = "label2";
+            label2.Size = new Size(50, 20);
+            label2.TabIndex = 8;
+            label2.Text = "INPUT";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(487, 222);
+            label3.Name = "label3";
+            label3.Size = new Size(64, 20);
+            label3.TabIndex = 9;
+            label3.Text = "OUTPUT";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(36, 105);
+            label4.Name = "label4";
+            label4.Size = new Size(90, 20);
+            label4.TabIndex = 10;
+            label4.Text = "KEY MATRIX";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1024, 458);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Margin = new Padding(3, 4, 3, 4);
+            Name = "Form1";
+            Text = "Playfair Cipher";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
---
+++ Playfair_cipher/Form1.cs
@@ -0,0 +1,129 @@
+﻿// File: Form1.cs
+using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ToolBar;
+
+namespace Playfair_cipher
+{
+    public partial class Form1 : Form
+    {
+        private char[,] keyTable = new char[5, 5];
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeEvents();
+        }
+
+        private void InitializeEvents()
+        {
+            // Gán sự kiện cho các nút
+            button1.Click += (sender, e) => Encrypt();
+            button2.Click += (sender, e) => Decrypt();
+            button3.Click += (sender, e) => ClearTextBoxes();
+        }
+
+        private void GenerateKeyTable(string key)
+        {
+            char[] alphabet = "ABCDEFGHIKLMNOPQRSTUVWXYZ".ToCharArray(); // Không bao gồm J
+            Regex rgx = new Regex("[^A-Z]");
+            key = rgx.Replace(key.ToUpper(), ""); // Xóa các ký tự không hợp lệ
+            key = new string(key.Distinct().ToArray()); // Loại bỏ ký tự trùng
+
+            // Loại bỏ các ký tự trong key ra khỏi bảng alphabet
+            foreach (char c in key)
+                alphabet = alphabet.Where(x => x != c).ToArray();
+
+            string keyString = key + new string(alphabet);
+
+            // Điền vào keyTable
+            for (int i = 0; i < 5; i++)
+                for (int j = 0; j < 5; j++)
+                    keyTable[i, j] = keyString[i * 5 + j];
+
+            // Hiển thị bảng mã trong textBox2
+            string keyMatrix = "";
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    keyMatrix += keyTable[i, j] + " ";
+                }
+                keyMatrix += Environment.NewLine;
+            }
+            textBox2.Text = keyMatrix;
+        }
+
+        private string ProcessText(string input, bool isEncrypt)
+        {
+            Regex rgx = new Regex("[^A-Z]");
+            input = rgx.Replace(input.ToUpper(), ""); // Chỉ giữ chữ cái
+            input = input.Replace('J', 'I'); // Thay thế J bằng I
+
+            // Nếu số ký tự lẻ, thêm 'X'
+            if (input.Length % 2 != 0) input += "X";
+
+            string result = "";
+            string keyString = string.Join("", keyTable.Cast<char>());
+
+            for (int i = 0; i < input.Length; i += 2)
+            {
+                int indexA = keyString.IndexOf(input[i]);
+                int indexB = keyString.IndexOf(input[i + 1]);
+                int rowA = indexA / 5, colA = indexA % 5;
+                int rowB = indexB / 5, colB = indexB % 5;
+
+                if (colA == colB) // Cùng cột
+                {
+                    result += keyTable[(rowA + (isEncrypt ? 1 : -1) + 5) % 5, colA];
+                    result += keyTable[(rowB + (isEncrypt ? 1 : -1) + 5) % 5, colB];
+                }
+                else if (rowA == rowB) // Cùng hàng
+                {
+                    result += keyTable[rowA, (colA + (isEncrypt ? 1 : -1) + 5) % 5];
+                    result += keyTable[rowB, (colB + (isEncrypt ? 1 : -1) + 5) % 5];
+                }
+                else // Thành hình chữ nhật
+                {
+                    result += keyTable[rowA, colB];
+                    result += keyTable[rowB, colA];
+                }
+            }
+
+            return result;
+        }
+
+        private void Encrypt()
+        {
+            string key = textBox1.Text;
+            string inputText = textBox3.Text;
+
+            GenerateKeyTable(key);
+            string encryptedText = ProcessText(inputText, true);
+
+            textBox4.Text = encryptedText;
+        }
+
+        private void Decrypt()
+        {
+            string key = textBox1.Text;
+            string inputText = textBox3.Text;
+
+            GenerateKeyTable(key);
+            string decryptedText = ProcessText(inputText, false);
+
+            textBox4.Text = decryptedText;
+        }
+
+        private void ClearTextBoxes()
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+        }
+    }
+}
